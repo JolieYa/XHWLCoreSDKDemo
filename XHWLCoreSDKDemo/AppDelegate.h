ab94466881931b30c601bc4ee756b4c055fbe1f5
511979194@qq.com <gongairong@iMac.local>
2017-12-19 16:24:09 +0800

    Initial Commit

---
+++ XHWLCoreSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHWLCoreSDKDemo
+//
+//  Created by gongairong on 2017/12/19.
+//  Copyright © 2017年 xinghaiwulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHWLCoreSDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHWLCoreSDKDemo
+//
+//  Created by gongairong on 2017/12/19.
+//  Copyright © 2017年 xinghaiwulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
